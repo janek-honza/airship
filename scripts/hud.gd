d57ea6d7ab03adb00df6bf932bfa91b97e249358
Janek <jasiek.owsiak@gmail.com>
2024-11-03 13:51:03 +0100

    Expanded upon basic flying model, made HUD elements anchor to the edges of the screen when it's resized, replaced .glb model with .blend file

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,8 +1,7 @@
 extends CanvasLayer
 
 @export var controls_hidden_status = false  # Changed variable name to follow convention and set default to true
-@onready var controls_shown = $ControlsShown
-@onready var controls_hidden = $ControlsHidden
+@onready var controls_cheatsheet = $Controls_cheatsheet
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -10,16 +9,27 @@ func _ready():
 
 func _input(event):
 	if Input.is_action_just_pressed("toggle_controls"):
-		controls_hidden_status = not controls_hidden_status  # Toggle the state
+		controls_hidden_status = not controls_hidden_status
 		update_controls_visibility()
 	
-	if Input.is_action_just_pressed("toggle_mouse"):
+	if Input.is_action_just_pressed("toggle_mouse"): #This is not an correct/clean way to do it, but Input.MOUSE_MODE is weird and other aproaches don't work
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func update_controls_visibility():
-	# Show/hide controls based on state
-	controls_shown.visible = not controls_hidden_status
-	controls_hidden.visible = controls_hidden_status
+	if controls_hidden_status == true:
+		controls_cheatsheet.text = "[b]Show/Hide controls cheatsheet: T[/b]"
+	else:
+		controls_cheatsheet.text = "[b]Show/Hide controls cheatsheet: T[/b]
+
+Inflate Ballonets (add mass): R
+Deflate Ballonets (reduce mass): F
+
+Increase/Decrease Throttle: Scroll wheel
+
+Pitch Up/Down: W/S
+Yaw Left/Right: A/D
+
+Free/Capture Mouse: Esc"
--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -2,14 +2,16 @@ extends CharacterBody3D
 
 @onready var Xpivot = $XPivot
 @onready var Ypivot = $XPivot/YPivot
-@onready var HUD = $HUD/Altitude_Throttle_Weight
+@onready var HUD = $HUD/Altitude_Throttle_Mass
 @onready var visuals = $visuals
 
 @export var sensitivity = 0.1
 @export var mass = 0
 @export var throttle = 0
-@export var left_engine_speed = 0
-@export var right_engine_speed = 0
+@export var left_engine_rpm = 0 #To be used in animation player
+@export var right_engine_rpm = 0
+
+@onready var Debug = $HUD/Debug
 
 #Vehicle specific modifiers
 var engine_efficiency = 0.25
@@ -22,6 +24,7 @@ var max_pitch_degree_in_pitch = 60 #Not really degrees, reaerch lerp()
 var pitch_level_speed = 1
 
 var fill_percent_ballonets = 40
+var target_fill_percent_ballonets = 40
 
 var throttle_max = 100
 var throttle_min = -50
@@ -31,6 +34,9 @@ var yaw_input = 0
 
 var cargo_weight = 100
 var displacment = 0
+var target_engine_speed = 0
+var engine_speed = 0
+var relative_speed_forward = velocity.dot(global_transform.basis.z)
 
 func _ready():
 	pass
@@ -41,34 +47,46 @@ func _input(event):
 		Ypivot.rotate_x(deg_to_rad(event.relative.y * sensitivity))
 	
 	if Input.is_action_pressed("throttle_up") and throttle < throttle_max:
-		throttle = throttle + 1
+		throttle = min(throttle + 1, 100)
 	
 	if Input.is_action_pressed("throttle_down") and throttle > throttle_min:
-		throttle = throttle - 1
+		throttle = max(throttle - 1, 0)
 	
 	pitch_input = Input.get_action_strength("pitch_up") - Input.get_action_strength("pitch_down")
 	
 	yaw_input = Input.get_action_strength("yaw_left") - Input.get_action_strength("yaw_right")
 	
-	if Input.is_action_pressed("inflate_ballonet") and fill_percent_ballonets < 100:
-		fill_percent_ballonets = fill_percent_ballonets + 1
+	if Input.is_action_pressed("inflate_ballonet"):
+		target_fill_percent_ballonets = min(target_fill_percent_ballonets + 1, 100)
 	
-	if Input.is_action_pressed("deflate_ballonet") and fill_percent_ballonets > 0:
-		fill_percent_ballonets = fill_percent_ballonets - 1
+	if Input.is_action_pressed("deflate_ballonet"):
+		target_fill_percent_ballonets = max(target_fill_percent_ballonets - 1, 0)
 
 func _process(delta):
-	mass = base_mass + cargo_weight + max_mass_of_ballonets * fill_percent_ballonets / 100
-	displacment = -self.global_transform.origin.y + 50 + base_mass + max_mass_of_ballonets * 0.5
-	HUD.text = "Altitude: " + str(self.global_transform.origin.y)+ "\nThrottle: " + str(throttle) + "\nMass: " + str(mass)
+	displacment = -self.global_transform.origin.y + 40 + base_mass + max_mass_of_ballonets * 0.5
+	HUD.text = "Altitude: " + str(snapped(self.global_transform.origin.y, 1))+ "\nThrottle: " + str(throttle) + "\nMass: " + str(snapped(mass, 1))
+	Debug.text = "Debug:" + "\n" + str(snapped(relative_speed_forward, 0.1)) + "\n" + str(snapped(engine_speed, 0.1))
 
-func _physics_process(delta: float):
-	var engine_direction = global_transform.basis.z
+func _physics_process(delta):
+	var engine_direction = global_transform.basis.z #in other airships engine direction can be changed
+	target_engine_speed = throttle * engine_efficiency
+	relative_speed_forward = velocity.dot(global_transform.basis.z)
+	if engine_speed <= target_engine_speed:
+		engine_speed = min(engine_speed + delta, target_engine_speed)
+	else:
+		engine_speed = max(engine_speed - delta, target_engine_speed)
 	
-	velocity = engine_direction * throttle * engine_efficiency + global_transform.basis.y * (displacment - mass)/10
+	if fill_percent_ballonets <= target_fill_percent_ballonets:
+		fill_percent_ballonets = min(fill_percent_ballonets + delta, target_fill_percent_ballonets)
+	else:
+		fill_percent_ballonets = max(fill_percent_ballonets - delta, target_fill_percent_ballonets)
+	mass = base_mass + cargo_weight + max_mass_of_ballonets * fill_percent_ballonets/100
 	
-	self.rotation.x = lerp(self.rotation.x, -float(pitch_input) * max_pitch_degree_in_pitch/360 * (self.velocity.z / 10), pitch_level_speed * delta)
+	velocity = engine_direction * engine_speed + Vector3.UP * (displacment - mass)/10
 	
-	self.rotation.y = self.rotation.y + yaw_input * yaw_speed
+	self.rotation.x = lerp(self.rotation.x, -float(pitch_input) * max_pitch_degree_in_pitch/360 * (relative_speed_forward / 10), pitch_level_speed * delta)
+	
+	self.rotation.y = self.rotation.y + yaw_input * yaw_speed * (5 / (abs(relative_speed_forward)+1))
 	visuals.rotation.z = lerp(visuals.rotation.z, -float(yaw_input) * max_roll_degree_in_yaw/360, roll_level_speed * delta)
 	
 	move_and_slide()
